cf3b80542b66c9acabda71ed3eb864f6206f5e4c
Iori Yanokura <ab.ioryz@gmail.com>
2024-10-27 20:10:13 +0900

    Import numpy as np because this is frequently used

--- ros/kxr_controller/scripts/interface.py
+++ ros/kxr_controller/scripts/interface.py
@@ -7,6 +7,7 @@ import argparse
 import IPython
 from kxr_controller.kxr_interface import KXRROSRobotInterface
 from kxr_models.download_urdf import download_urdf_mesh_files
+import numpy as np  # NOQA
 import rospy
 from skrobot.model import RobotModel
 
