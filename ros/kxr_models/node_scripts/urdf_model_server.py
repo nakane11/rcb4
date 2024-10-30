dd574a8dde552660bf8a0cc011da39542338d719
Iori Yanokura <ab.ioryz@gmail.com>
2024-10-30 13:14:18 +0900

    Add suffix for temp urdf

--- ros/kxr_models/node_scripts/urdf_model_server.py
+++ ros/kxr_models/node_scripts/urdf_model_server.py
@@ -36,7 +36,8 @@ class URDFModelServer:
                 continue
 
             previous_urdf = urdf
-            with tempfile.NamedTemporaryFile("w", delete=False) as temp_file:
+            with tempfile.NamedTemporaryFile("w", suffix='.urdf',
+                                             delete=False) as temp_file:
                 temp_file.write(urdf)
                 urdf_path = temp_file.name
 
